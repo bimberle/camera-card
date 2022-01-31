4a46855037d35fbd1f90fafac93848c009472df8
bimberle <31180705+bimberle@users.noreply.github.com>
2022-01-31 15:59:45 +0100

    Add files via upload

--- src/custom-element/my-custom-card.ts
+++ src/custom-element/my-custom-card.ts
@@ -1,7 +1,6 @@
 import { HomeAssistant } from "../ha-types";
 import { html, css, LitElement, CSSResultGroup, TemplateResult, PropertyValues } from "lit";
 import { property } from "lit/decorators";
-import {repeat} from 'lit-html/directives/repeat.js';
 import { Cam, ICardConfig, Picture, PictureGallery, Tag } from "../types";
 import styles from "./card.css";
 import { hasConfigOrEntityChanged } from "../has-changed";
@@ -24,11 +23,10 @@ export class MyCustomCard extends LitElement {
 
     @property() private _config?: ICardConfig;
 
-    @property({ type: Number }) selectedTab: number = 0;
-    @property({ type: String }) selectedDay: string = ""
-    @property({ type: String }) selectedHour: string = "";
-    @property({ type: String }) selectedCam: string = "";
-    @property({ type: String }) selectedPic: string = "";
+    private selectedDay: string = ""
+    private selectedHour: string = "";
+    private selectedCam: string = "";
+    private selectedPic: string = "";
 
     private entityObj: any = undefined;
     private gallery: any = undefined;
@@ -56,25 +54,26 @@ export class MyCustomCard extends LitElement {
     }
 
     private _selectHour(hour: string) :void {
+        console.log("Hour selected:" + hour);
         this.selectedHour = hour;
         this.availableCams = Object.keys(this.gallery[this.selectedDay][this.selectedHour]) as Array<string>;
-        this._selectCam(this.availableCams[0]);
+        if(this.selectedCam != this.availableCams[0])
+            this._selectCam(this.availableCams[0]);
     }
 
     private _selectCam(cam: string) :void {
         this.selectedCam = cam;
-        //this.availablePics = [];
+        const newPics: Array<Picture> = [];
+        //this.availablePics.splice(0, this.availablePics.length);
         Object.entries(this.gallery[this.selectedDay][this.selectedHour][this.selectedCam]).forEach((thisobj, index) => {
             var picurl = this.localpath + thisobj[1];
-            this.availablePics.push({ timestamp: thisobj[0], url: picurl})
+            newPics.push({ timestamp: thisobj[0], url: picurl})
         })
 
         // TODO
         // this.availablePics kommt nicht bei HTML an, dort ist das Object noch alt
 
-
-        if(this.availablePics.length > 0)
-            this._selectPic(this.availablePics[0]);
+        this.availablePics = newPics;
     }
 
     private _selectPic(pic: Picture) : void {
@@ -132,43 +131,10 @@ export class MyCustomCard extends LitElement {
                         </div>
                     </div>
                     <div class="card-content">
-            
-                        <div id='days'>
-                            <paper-tabs scrollable class='smallTabs'>
-                            ${this.availableDays?.map((tag: string) => {
-                                return html`
-                                <paper-tab @click=${() => this._selectDay(tag)}>${tag}</paper-tab>
-                                `;
-                            })}
-                            </paper-tabs>
-                        </div>
-
-                        <div id='hours'>
-                            <paper-tabs scrollable class='smallTabs'>
-                                ${this.availableHours?.map((hour: string) => {
-                                    return html`
-                                        <paper-tab @click=${() => this._selectHour(hour)}>${hour}</paper-tab>
-                                        `;
-                                        })}
-                            </paper-tabs>
-                        </div>
-
-                        <div id='cams'>
-                            <paper-tabs scrollable class='smallTabs'>
-                                ${this.availableCams?.map((cam: string) => {
-                                    return html`
-                                        <paper-tab @click=${() => this._selectCam(cam)}>${cam}</paper-tab>
-                                        `;
-                                        })}
-                            </paper-tabs>
-                        </div>
-
-                        <div id='pics'>
-                                ${this.availablePics.map((pic: Picture) => {
-                                    return html`<div class='previews'><img src=${pic.url} class='previews'></img><div class='previewtimestamp'>${pic.timestamp}</div></div>`;
-                                    })}
-                            
-                        </div>
+                        ${this.renderDays()}
+                        ${this.renderHours()}
+                        ${this.renderCams()}
+                        ${this.availablePics.length > 0 ? this.renderPictures() : ''}
                     </div>
 
                 </ha-card>
@@ -177,31 +143,74 @@ export class MyCustomCard extends LitElement {
         else
         {
             return html`<div>Loading...</div>`;
-        }
-        
+        }   
+    }
 
-        
+    private renderDays(): TemplateResult
+    {
+        return html`
+        <div id='days'>
+            <paper-tabs scrollable class='smallTabs'>
+            ${this.availableDays?.map((tag: string) => {
+                return html`
+                <paper-tab @click=${() => this._selectDay(tag)}>${tag}</paper-tab>
+                `;
+            })}
+            </paper-tabs>
+        </div>
+        `;
     }
+
+    private renderHours(): TemplateResult
+    {
+        return html`
+        <div id='hours'>
+            <paper-tabs scrollable class='smallTabs'>
+                ${this.availableHours?.map((hour: string) => {
+                    return html`
+                        <paper-tab @click=${() => this._selectHour(hour)}>${hour}</paper-tab>
+                        `;
+                        })}
+            </paper-tabs>
+        </div>
+        `;
+    }
+    private renderCams(): TemplateResult
+    {
+        return html`
+        <div id='cams'>
+            <paper-tabs scrollable class='smallTabs'>
+                ${this.availableCams?.map((cam: string) => {
+                    return html`
+                        <paper-tab @click=${() => this._selectCam(cam)}>${cam}</paper-tab>
+                        `;
+                        })}
+            </paper-tabs>
+        </div>
+        `;
+    }
+
+    private renderPictures(): TemplateResult 
+    {
+        console.log("Pictures: " + this.availablePics.length);
+        return html`
+            <div id='pics'>
+                ${this.availablePics.map((pic: Picture) => {
+                    return html`<div class='previews'><img src=${pic.url} class='previews'><div class='previewtimestamp'>${pic.timestamp}</div></div>`;
+                    })}
+            
+            </div>
+        `;
+    }
+
     protected shouldUpdate(changedProps: PropertyValues): boolean {
-        if (changedProps.has("fileList")) {
-            return true;
-        }
-    
         return hasConfigOrEntityChanged(this, changedProps);
-        }
-    
-    protected updated(changedProps: PropertyValues) {
-        super.updated(changedProps);
-    
-        if (changedProps.has("hass")) {
-            const stateObj = this.hass!.states[this._config!.entity];
-            const oldHass = changedProps.get("hass") as this["hass"];
-            const oldStateObj = oldHass
-            ? oldHass.states[this._config!.entity]
-            : undefined;
-    
-        }
-    }
+      }
+
+
+    //protected updated(changedProperties: PropertyValues): void {
+    //    super.updated(changedProperties);
+    //}
 
 }
 
